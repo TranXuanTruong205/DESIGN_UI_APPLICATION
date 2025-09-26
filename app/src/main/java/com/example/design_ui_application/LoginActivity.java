bb87cdaa7019337aa621566e41c8883ec0d6ce2d
TranXuanTruong205 <truong2005tran@gmail.com>
2025-09-26 23:28:57 +0700

    Complete Design Login/Regsister UI

--- app/src/main/java/com/example/design_ui_application/LoginActivity.java
+++ app/src/main/java/com/example/design_ui_application/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.design_ui_application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,14 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        TextView btn_2339 = findViewById(R.id.txtSignUp);
+        btn_2339.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent_2339 = new Intent(LoginActivity.this,RegsisterActivity.class);
+                startActivity(myintent_2339);
+            }
         });
     }
 }
--- app/src/main/java/com/example/design_ui_application/RegsisterActivity.java
+++ app/src/main/java/com/example/design_ui_application/RegsisterActivity.java
@@ -1,6 +1,10 @@
 package com.example.design_ui_application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,14 @@ public class RegsisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_regsister);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btn_2339 = findViewById(R.id.btnLogin);
+        btn_2339.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent_2339 = new Intent(RegsisterActivity.this, LoginActivity.class);
+                startActivity(myintent_2339);
+            }
         });
     }
+
 }
